92ab2831c41e2b2e808ade5de9fb2c338152cab7
Bruno <bruno9405@fito.br>
2025-02-04 14:41:18 -0300

    Adicionando Conteúdo(avaliação) e backend

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 cloudinary.config( 
-  cloud_name = os.getenv("CLOUND_NAME"), 
+  cloud_name = os.getenv("CLOUD_NAME"), 
   api_key = os.getenv("API_KEY"), 
   api_secret = os.getenv("API_SECRET") 
 )
